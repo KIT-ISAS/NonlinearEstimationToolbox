ad81f340a057ca87232d8a70c8a919d74057f94e
Jannik Steinbring <jannik.steinbring@kit.edu>
2015-08-03 11:17:32 +0200

    Adds a wrapper function for the GLCD MEX files. The wrapper function checks if the platform and the used MATLAB version are supported and calls the specific MEX file.

---
+++ Matlab/Toolbox/Misc/GLCD.m
@@ -0,0 +1,53 @@
+
+function varargout = GLCD(varargin)
+    % Wrapper function to call the platform-specific GLCD MEX files.
+    %
+    % Execute GLCD without parameters to get more information.
+    
+    % >> This function/class is part of the Nonlinear Estimation Toolbox
+    %
+    %    For more information, see https://bitbucket.org/nonlinearestimation/toolbox
+    %
+    %    Copyright (C) 2015  Jannik Steinbring <jannik.steinbring@kit.edu>
+    %
+    %                        Institute for Anthropomatics and Robotics
+    %                        Chair for Intelligent Sensor-Actuator-Systems (ISAS)
+    %                        Karlsruhe Institute of Technology (KIT), Germany
+    %
+    %                        http://isas.uka.de
+    %
+    %    This program is free software: you can redistribute it and/or modify
+    %    it under the terms of the GNU General Public License as published by
+    %    the Free Software Foundation, either version 3 of the License, or
+    %    (at your option) any later version.
+    %
+    %    This program is distributed in the hope that it will be useful,
+    %    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    %    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    %    GNU General Public License for more details.
+    %
+    %    You should have received a copy of the GNU General Public License
+    %    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+    
+    varargout = cell(1, nargout);
+    
+    switch computer()
+        case 'GLNXA64'
+            if verLessThan('matlab', '8.2')
+                error('GLCD:UnsupportedMatlabVersion', ...
+                      'At least Matlab R2013b is required.');
+            else
+                [varargout{:}] = GLCD_R2013b(varargin{:});
+            end
+        case 'PCWIN64'
+            if verLessThan('matlab', '8.3')
+                error('GLCD:UnsupportedMatlabVersion', ...
+                      'At least Matlab R2014a is required.');
+            else
+                [varargout{:}] = GLCD_R2014a(varargin{:});
+            end
+        otherwise
+            error('GLCD:UnsupportedPlatform', ...
+                  'Only Windows 64-bit and Linux are supported.');
+    end
+end
