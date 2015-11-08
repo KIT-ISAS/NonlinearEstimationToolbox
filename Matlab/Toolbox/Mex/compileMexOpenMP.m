b3a369e3ccaec1f7bf82d41156161fa63ffbfaa6
Jannik Steinbring <jannik.steinbring@kit.edu>
2015-11-08 18:13:58 +0100

    Add compileMexOpenMP() function that automatically enables OpenMP when compiling a MEX file.

---
+++ Matlab/Toolbox/Mex/compileMexOpenMP.m
@@ -0,0 +1,41 @@
+
+function compileMexOpenMP(varargin)
+    % Compile a MEX binary with OpenMP support enabled
+    
+    % >> This function/class is part of the Nonlinear Estimation Toolbox
+    %
+    %    For more information, see https://bitbucket.org/nonlinearestimation/toolbox
+    %
+    %    Copyright (C) 2015  Jannik Steinbring <jannik.steinbring@kit.edu>
+    %
+    %                        Institute for Anthropomatics and Robotics
+    %                        Chair for Intelligent Sensor-Actuator-Systems (ISAS)
+    %                        Karlsruhe Institute of Technology (KIT), Germany
+    %
+    %                        http://isas.uka.de
+    %
+    %    This program is free software: you can redistribute it and/or modify
+    %    it under the terms of the GNU General Public License as published by
+    %    the Free Software Foundation, either version 3 of the License, or
+    %    (at your option) any later version.
+    %
+    %    This program is distributed in the hope that it will be useful,
+    %    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    %    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    %    GNU General Public License for more details.
+    %
+    %    You should have received a copy of the GNU General Public License
+    %    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+    
+    % Enable OpenMP
+    if ispc()
+        % On Windows
+        options = { 'OPTIMFLAGS=$OPTIMFLAGS /openmp' };
+    else
+        % On Linux and Mac OS
+        options = { 'CXXFLAGS=$CXXFLAGS -fopenmp', ...
+                    'LDFLAGS=$LDFLAGS -fopenmp' };
+    end
+    
+    compileMex(varargin{:}, options{:});
+end
