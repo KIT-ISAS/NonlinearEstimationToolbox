a587ce1329649a0dc1db0d64d73ab611833ef85c
Jannik Steinbring <jannik.steinbring@kit.edu>
2017-07-29 16:13:49 +0200

    GaussianSamplingRnd: increase default number of samples to 1,000.

--- Matlab/Toolbox/GaussianSampling/GaussianSamplingRnd.m
+++ Matlab/Toolbox/GaussianSampling/GaussianSamplingRnd.m
@@ -43,7 +43,7 @@ classdef GaussianSamplingRnd < GaussianSampling
             %   << obj (GaussianSamplingRnd)
             %      A new GaussianSamplingRnd instance.
             
-            obj.numSamples = 100;
+            obj.numSamples = 1000;
         end
         
         function setNumSamples(obj, numSamples)
